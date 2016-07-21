cba352692887f2527ca239eba877bc2fabdf0ae5
PedroDominciano <pedrocb82@gmail.com>
2016-07-21 20:16:30 -0300

    verificação att
    colocado mais verificações no codigo

--- js/verify.js
+++ js/verify.js
@@ -1,25 +1,40 @@
 function verificar(){
 	var formulario = document.getElementById("formulario");
-	var nome = formulario.nome.value;
-	var sobreNome = formulario.sobrenome.value;
-	var email = formulario.email.value;
-	var assunto = formulario.ass.value;
-
-	if(nome.length > 10 || sobreNome.length > 10 || assunto.length > 16){
-		console.log("Os campos podem ter,no maximo, 8 caracteres")
-		document.getElementById("nome");
-	}
+	valida_nomes(formulario);
+	
 	if(!valida_form()){
-		document.getElementById("email").focus();
+		document.getElementById("inp-email").focus();
 	}
 }
 
 function valida_form (){
 	var filter = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-	if(!filter.test(document.getElementById("email").value))
+	if(!filter.test(document.getElementById("inp-email").value))
 	{
 		console.log("Favor verificar o email");
 		return false
 	}
 	return true;
+}
+
+function valida_nomes(formulario){	
+	var regex = /\D{2,}/;
+	var nome = formulario.nome.value;
+	var sobreNome = formulario.sobrenome.value;
+	var assunto = formulario.ass.value;
+	if(!regex.test(nome)){
+		alert("Favor verificar o nome");
+		document.getElementById("inp-nome").value='';
+		document.getElementById("inp-nome").style.backgroundColor = "#FF6666";
+	}
+	if(!regex.test(sobreNome)){
+		alert("Favor verificar o Sobrenome");
+		document.getElementById("inp-sobreNome").value='';
+		document.getElementById("inp-sobreNome").style.backgroundColor = "#FF6666";
+	}
+	if(!regex.test(assunto)){
+		alert("Favor verificar o Assunto");
+		document.getElementById("inp-ass").value='';
+		document.getElementById("inp-ass").style.backgroundColor = "#FF6666";
+	}
 }
